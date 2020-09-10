1eb557481d800caebd3d9e882be270662d63a362
Abdullah06444 <abdullah44gur@gmail.com>
2020-09-10 10:47:40 +0300

    initial commit

---
+++ CSharp0013_PythagoreanTriples.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CSharp0013_PythagoreanTriples
+{
+    class CSharp0013_PythagoreanTriples
+    {
+        static void Main(string[] args)
+        {
+
+            int side1, side2;
+            bool flag = true;
+
+            // 353'ten sonrakilerde hipotenus 500'un ustune cikiyor
+            for (side1 = 1; side1 < 354; side1++)
+            {
+
+                for (side2 = side1; flag; side2++)
+                {
+
+                    int result = (int) (Math.Pow(side1, 2) + Math.Pow(side2, 2));
+                    
+                    if(Math.Sqrt(result) % 1 == 0)
+                        Console.WriteLine("Side1 => " + side1 + "\tSide2 => " + side2 + "\tHypotenuse => " + Math.Sqrt(result));
+
+                    if (Math.Sqrt(result) > 500)
+                        flag = false;
+                }
+                flag = true;
+            }
+        }
+    }
+}
